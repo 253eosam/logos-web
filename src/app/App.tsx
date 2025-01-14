0f7cec1c7c65b85b95cba211975c83070ed43766
Sungjun Lee <253eosam@gmail.com>
2025-01-14 17:44:35 +0900

    chroe: install eslint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,15 @@
-export default function App () {
-  return <>
-    <h1 className="text-red-600 text-[2.5rem]">
-      Pretendard
-    </h1>
+export default function App() {
+  console.log(1)
+  console.log('2')
+  console.log('3')
 
-    <p className="text-blue-600 text-[1.5rem]">
-      other font 
-    </p>
-  </>
+  
+  
+  
+  return (
+    <>
+      <h1 className="text-red-600 text-[2.5rem]">Pretendard</h1>
+      <p className="text-blue-600 text-[1.5rem]">other font</p>
+    </>
+  )
 }
