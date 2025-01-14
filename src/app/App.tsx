fc3c700b5f20da57d6c6392fbfbd2fd1e45819b2
Sungjun Lee <253eosam@gmail.com>
2025-01-14 16:07:16 +0900

    chroe: install tailwind
    - font family : pretendard

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+export default function App () {
+  return <>
+    <h1 className="text-red-600 text-[2.5rem]">
+      Pretendard
+    </h1>
+
+    <p className="text-blue-600 text-[1.5rem]">
+      other font 
+    </p>
+  </>
+}
