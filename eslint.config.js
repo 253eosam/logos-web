47af5d8ee2662d96c756bba6fd06e1877f1a4bbc
Sungjun Lee <253eosam@gmail.com>
2025-01-14 18:14:33 +0900

    chroe: install prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -10,7 +11,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-
+  eslintPluginPrettierRecommended,
   {
     rules: {
       'react/react-in-jsx-scope': 'off',
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,10 @@ export default function App() {
   console.log('2')
   console.log('3')
 
-  
-  
-  
   return (
     <>
-      <h1 className="text-red-600 text-[2.5rem]">Pretendard</h1>
-      <p className="text-blue-600 text-[1.5rem]">other font</p>
+      <h1 className='text-red-600 text-[2.5rem]'>Pretendard</h1>
+      <p className='text-blue-600 text-[1.5rem]'>other font</p>
     </>
   )
 }
